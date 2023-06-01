0596d8bb4508be3ef8ebbe47921a8cfeef717332
Yao <meizhen.yao@mssm.edu>
2023-06-01 14:31:20 -0400

    updates

--- child growth standards calculation/S-PRESTO_child_anthropometry_WHO_standards_v2.R
+++ child growth standards calculation/S-PRESTO_child_anthropometry_WHO_standards_v2.R
@@ -11,6 +11,9 @@ library(readr)
 library(mice)
 library(anthro)
 library(writexl)
+library(ggh4x)
+library(naniar)
+library(UpSetR)
 
 #-------------------------------------------------------import data set
 whole_cohort<-  read_csv("C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/S-PRESTO/input/for paper/whole_cohort_20230316.csv")
@@ -33,68 +36,61 @@ blood$length_m12<- ifelse(blood$Subject_ID==10573, NA, blood$length_m12)
 ## add days_since_birth_del_CN variable
 blood$Days_since_birth_del_CN<- rep(0, 328)
 
+
+blood$Days_since_birth_del<- ifelse(blood$Days_since_birth_del == -1, NA, blood$Days_since_birth_del)
+blood$weight_del<- ifelse(blood$Days_since_birth_del == -1, NA, blood$weight_del)
+blood$length_del<- ifelse(blood$Days_since_birth_del == -1, NA, blood$length_del)
+
+## merge variables at delivery
+del<- blood[, c('weight_del', 'length_del', 'Days_since_birth_del')]
+
+blood<- blood %>%
+        mutate(weight_del_merge = coalesce(weight_del, weight_del_CN),
+               length_del_merge = coalesce(length_del, length_del_CN),
+               Days_since_birth_del_merge = if_else(is.na(weight_del) == TRUE, 0, Days_since_birth_del))
+
+
+merge<- blood[, c('weight_del_merge', 'length_del_merge', 'Days_since_birth_del_merge')]
+
+
+
+
+
+
+
 ## only include child anthropometry variables
-weight<- c('weight_del_CN','weight_del', 'weight_wk1','weight_wk3','weight_wk6','weight_m3','weight_m6','weight_m12','weight_m18')
-length<- c('length_del_CN','length_del', 'length_wk1','length_wk3','length_wk6','length_m3','length_m6','length_m12','length_m18')
-child_age<- c('Days_since_birth_del_CN','Days_since_birth_del','Days_since_birth_wk1','Days_since_birth_wk3','Days_since_birth_wk6','Days_since_birth_m3','Days_since_birth_m6','Days_since_birth_m12','Days_since_birth_m18')
+weight<- c('weight_del_merge', 'weight_wk1','weight_wk3','weight_wk6','weight_m3','weight_m6','weight_m12','weight_m18')
+length<- c('length_del_merge', 'length_wk1','length_wk3','length_wk6','length_m3','length_m6','length_m12','length_m18')
+child_age<- c('Days_since_birth_del_merge','Days_since_birth_wk1','Days_since_birth_wk3','Days_since_birth_wk6','Days_since_birth_m3','Days_since_birth_m6','Days_since_birth_m12','Days_since_birth_m18')
 
 
 blood_child<- blood[,c("Subject_ID", "bw_sex", "bw_birth_GA", weight, length, child_age,'age_at_recruitment', 'pcv1_highest_education_completed_recat','ethnicity_specified_recat','pcv1_parity_recat','pcv1_bmi')]
+blood_child_del_m18<- blood_child[complete.cases(blood_child[,c('weight_del_merge', 'weight_m18', "length_del_merge", "length_m18", "Days_since_birth_del_merge", "Days_since_birth_m18")]),]
+
+vis_miss(blood_child_del_m18)
+vis_miss(blood_child_del_m18,sort_miss = TRUE)
+gg_miss_var(blood_child_del_m18)
 
 
-## assign NA to '-1' in days_since_Del
-blood_child$Days_since_birth_del<- ifelse(blood_child$Days_since_birth_del == -1, NA, blood_child$Days_since_birth_del)
+blood_child_complete<- blood_child[complete.cases(blood_child),]
 
-## Restricted to participants who came at least one visit
-### weights
-blood_child = blood_child[!(is.na(blood_child$weight_del_CN)  & 
-                              is.na(blood_child$weight_del) &
-                              is.na(blood_child$weight_wk1) &
-                              is.na(blood_child$weight_wk3) &
-                              is.na(blood_child$weight_wk6) &
-                              is.na(blood_child$weight_m3) &
-                              is.na(blood_child$weight_m6) &
-                              is.na(blood_child$weight_m12) &
-                              is.na(blood_child$weight_m18)), ]
-### lengths
-blood_child = blood_child[!(is.na(blood_child$length_del_CN)  &
-                              is.na(blood_child$length_del) &
-                              is.na(blood_child$length_wk1) &
-                              is.na(blood_child$length_wk3) &
-                              is.na(blood_child$length_wk6) &
-                              is.na(blood_child$length_m3) &
-                              is.na(blood_child$length_m6) &
-                              is.na(blood_child$length_m12) &
-                              is.na(blood_child$length_m18)), ]
-### days since birth
-blood_child = blood_child[!(is.na(blood_child$Days_since_birth_del_CN) & 
-                              is.na(blood_child$Days_since_birth_del) &
-                              is.na(blood_child$Days_since_birth_wk1) &
-                              is.na(blood_child$Days_since_birth_wk3) &
-                              is.na(blood_child$Days_since_birth_wk6) &
-                              is.na(blood_child$Days_since_birth_m3) &
-                              is.na(blood_child$Days_since_birth_m6) &
-                              is.na(blood_child$Days_since_birth_m12) &
-                              is.na(blood_child$Days_since_birth_m18)), ]
-
-# write_csv(blood_child, "C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/S-PRESTO/input/anthropometry/blood_child_before_imputed.csv")
 
 #------------------------------------------------------- imputation
 ###------------------------------------------------------------------------------ weights & length & bw_sex & GA
-str(blood_child)
+str(blood_child_del_m18)
 
-blood_child$pcv1_highest_education_completed_recat<- as.factor(blood_child$pcv1_highest_education_completed_recat)
-blood_child$ethnicity_specified_recat<- as.factor(blood_child$ethnicity_specified_recat)
-blood_child$pcv1_parity_recat<- as.factor(blood_child$pcv1_parity_recat)
+blood_child_del_m18$pcv1_highest_education_completed_recat<- as.factor(blood_child_del_m18$pcv1_highest_education_completed_recat)
+blood_child_del_m18$ethnicity_specified_recat<- as.factor(blood_child_del_m18$ethnicity_specified_recat)
+blood_child_del_m18$pcv1_parity_recat<- as.factor(blood_child_del_m18$pcv1_parity_recat)
 
-str(blood_child)
+str(blood_child_del_m18)
 #### to check missingness
 
-blood_child_for_impute<- subset(blood_child, select = -(Subject_ID))
-apply(blood_child_for_impute, 2, function(x) sum(is.na(x))) 
-md.pattern(blood_child_for_impute)
+blood_child_del_m18_for_impute<- subset(blood_child_del_m18, select = -(Subject_ID))
+apply(blood_child_del_m18_for_impute, 2, function(x) sum(is.na(x))) 
+md.pattern(blood_child_del_m18_for_impute)
 #### Initialize the Imputation
-init = mice(blood_child_for_impute, maxit=0) 
+init = mice(blood_child_del_m18_for_impute, maxit=0) 
 meth = init$method
 predM = init$predictorMatrix
 
@@ -102,17 +98,17 @@ predM = init$predictorMatrix
 
 ####  Specify a method to be used for imputation for some selected variables 
 #####   used Random Forest(rf) for selected variables, and blank for other variables
-meth[c('age_at_recruitment', 'pcv1_highest_education_completed_recat','ethnicity_specified_recat','pcv1_parity_recat')] = ""
+meth[c('age_at_recruitment', 'pcv1_highest_education_completed_recat','ethnicity_specified_recat','pcv1_parity_recat', "bw_sex", "bw_birth_GA", "pcv1_bmi")] = ""
 
-meth[c(weight, length, child_age, "bw_sex", "bw_birth_GA", "pcv1_bmi")] = "rf"
+meth[c(weight, length, child_age)] = "rf"
 
 
 ####   Now Run your Main Imputation using "Random Forest"
 #####   m = 5 creates 5 different imputed datasets 
 set.seed(951263)
-blood_child_imputing = mice(blood_child_for_impute, method=meth, predictorMatrix=predM, m=8, ntree = 50)
+blood_child_del_m18_imputing = mice(blood_child_del_m18_for_impute, method=meth, predictorMatrix=predM, m=8, ntree = 50)
 #####    Use the last completed data set 
-blood_child_imputed <- complete(blood_child_imputing, action=8)
+blood_child_imputed <- complete(blood_child_del_m18_imputing, action=8)
 
 
 ####    Check if you actually imputed all the variables you wanted to impute
@@ -120,28 +116,19 @@ sapply(blood_child_imputed, function(x) sum(is.na(x)))
 
 
 
-blood_child_imputed<- data.frame(blood_child[, c("Subject_ID")],
+blood_child_imputed<- data.frame(blood_child_del_m18[, c("Subject_ID")],
                                  blood_child_imputed) %>% 
                       mutate(measure="L")
 
-cor(blood_child_imputed$weight_del,blood_child_imputed$weight_del_CN)
-cor(blood_child_imputed$length_del,blood_child_imputed$length_del_CN)
 
 # write_csv(blood_child_imputed, "C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/S-PRESTO/input/anthropometry/blood_child_imputed.csv")
 
 
 #------------------------------------------------------- restrict to completed cases
-weight_CN<- c('weight_del', 'weight_wk1','weight_wk3','weight_wk6','weight_m3','weight_m6','weight_m12','weight_m18')
-length_CN<- c('length_del', 'length_wk1','length_wk3','length_wk6','length_m3','length_m6','length_m12','length_m18')
-child_age_CN<- c('Days_since_birth_del','Days_since_birth_wk1','Days_since_birth_wk3','Days_since_birth_wk6','Days_since_birth_m3','Days_since_birth_m6','Days_since_birth_m12','Days_since_birth_m18')
-
-
-blood_child_complete<- subset(blood_child[complete.cases(blood_child[, c(weight, length, child_age)]),], select=-c(weight_del_CN, length_del_CN, Days_since_birth_del_CN))
-
 colSums(is.na(blood_child_complete))
 
 blood_child_complete<- blood_child_complete %>% 
-                       mutate(measure="L")
+  mutate(measure="L")
 
 # write_csv(blood_child_complete, "C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/S-PRESTO/input/anthropometry/blood_child_complete.csv")
 
@@ -150,32 +137,19 @@ blood_child_complete<- blood_child_complete %>%
 
 #------------------------------------------------------- WHO child growth standards
 #----------------- imputed data
-# WHO child growth standards
-blood_child_imputed<- read_csv("C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/S-PRESTO/input/anthropometry/blood_child_imputed.csv")
 
-## delivery (hospital records)
-blood_child_who_stan_del_CN<- with(
+## delivery (merge hospital records & delivery visit)
+blood_child_who_stan_del_merge<- with(
   blood_child_imputed,
   anthro_zscores(sex = bw_sex, 
-                 age = Days_since_birth_del_CN, 
-                 weight = weight_del_CN/1000, 
-                 lenhei = length_del_CN,
+                 age = Days_since_birth_del_merge, 
+                 weight = weight_del_merge/1000, 
+                 lenhei = length_del_merge,
                  measure = measure)
 )[,c("zwei", "zlen", "zbmi", "zwfl")] 
 
-names(blood_child_who_stan_del_CN)<- c("zweight_del_CN", "zlength_del_CN", "zbmi_del_CN", "zwfl_WHO_del_CN")
+names(blood_child_who_stan_del_merge)<- c("zweight_del_merge", "zlength_del_merge", "zbmi_del_merge", "zwfl_WHO_del_merge")
 
-## delivery
-blood_child_who_stan_del<- with(
-  blood_child_imputed,
-  anthro_zscores(sex = bw_sex, 
-                 age = Days_since_birth_del, 
-                 weight = weight_del/1000, 
-                 lenhei = length_del,
-                 measure = measure)
-)[,c("zwei", "zlen", "zbmi", "zwfl")] 
-
-names(blood_child_who_stan_del)<- c("zweight_del", "zlength_del", "zbmi_del", "zwfl_WHO_del")
 
 ## at wk1
 blood_child_who_stan_wk1<- with(
@@ -264,8 +238,7 @@ names(blood_child_who_stan_m18)<- c("zweight_m18", "zlength_m18", "zbmi_m18", "z
 
 # Internal z-scores for weight-to-height ratio
 wfl_internal_zscores<- blood_child_imputed %>% 
-  transmute(zwfl_internal_del_CN = (weight_del_CN/(1000*length_del_CN) - mean(weight_del_CN/(1000*length_del_CN)))/sd(weight_del_CN/(1000*length_del_CN)),
-            zwfl_internal_del = (weight_del/(1000*length_del) - mean(weight_del/(1000*length_del)))/sd(weight_del/(1000*length_del)),
+  transmute(zwfl_internal_del_merge = (weight_del_merge/(1000*length_del_merge) - mean(weight_del_merge/(1000*length_del_merge)))/sd(weight_del_merge/(1000*length_del_merge)),
             zwfl_internal_wk1 = (weight_wk1/(1000*length_wk1) - mean(weight_wk1/(1000*length_wk1)))/sd(weight_wk1/(1000*length_wk1)),
             zwfl_internal_wk3 = (weight_wk3/(1000*length_wk3) - mean(weight_wk3/(1000*length_wk3)))/sd(weight_wk3/(1000*length_wk3)),
             zwfl_internal_wk6 = (weight_wk6/(1000*length_wk6) - mean(weight_wk6/(1000*length_wk6)))/sd(weight_wk6/(1000*length_wk6)),
@@ -276,8 +249,7 @@ wfl_internal_zscores<- blood_child_imputed %>%
 
 ## combine all datasets and add 
 blood_child_who_stan<- data.frame(blood_child_imputed[,c("Subject_ID", weight, length, child_age, "bw_sex", "bw_birth_GA")],
-                                  blood_child_who_stan_del_CN,
-                                  blood_child_who_stan_del,
+                                  blood_child_who_stan_del_merge,
                                   blood_child_who_stan_wk1,
                                   blood_child_who_stan_wk3,
                                   blood_child_who_stan_wk6,
@@ -289,23 +261,22 @@ blood_child_who_stan<- data.frame(blood_child_imputed[,c("Subject_ID", weight, l
 
 
 
-# write_csv(blood_child_who_stan, "C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/S-PRESTO/input/anthropometry/blood_child_who_stan_04172023.csv")
+# write_csv(blood_child_who_stan, "C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/S-PRESTO/input/anthropometry/blood_child_who_stan_imputed_05262023.csv")
 
 #----------------- complete cases data
-# WHO child growth standards
-blood_child_complete<- read_csv("C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/S-PRESTO/input/anthropometry/blood_child_complete.csv")
 
-## delivery (hospital records)
-blood_child_who_stan_del<- with(
+## delivery (merge hospital records & delivery visit)
+blood_child_who_stan_del_merge<- with(
   blood_child_complete,
   anthro_zscores(sex = bw_sex, 
-                 age = Days_since_birth_del, 
-                 weight = weight_del/1000, 
-                 lenhei = length_del,
+                 age = Days_since_birth_del_merge, 
+                 weight = weight_del_merge/1000, 
+                 lenhei = length_del_merge,
                  measure = measure)
 )[,c("zwei", "zlen", "zbmi", "zwfl")] 
 
-names(blood_child_who_stan_del)<- c("zweight_del", "zlength_del", "zbmi_del", "zwfl_WHO_del")
+names(blood_child_who_stan_del_merge)<- c("zweight_del_merge", "zlength_del_merge", "zbmi_del_merge", "zwfl_WHO_del_merge")
+
 
 ## at wk1
 blood_child_who_stan_wk1<- with(
@@ -394,7 +365,7 @@ names(blood_child_who_stan_m18)<- c("zweight_m18", "zlength_m18", "zbmi_m18", "z
 
 # Internal z-scores for weight-to-height ratio
 wfl_internal_zscores<- blood_child_complete %>% 
-  transmute(zwfl_internal_del = (weight_del/(1000*length_del) - mean(weight_del/(1000*length_del)))/sd(weight_del/(1000*length_del)),
+  transmute(zwfl_internal_del_merge = (weight_del_merge/(1000*length_del_merge) - mean(weight_del_merge/(1000*length_del_merge)))/sd(weight_del_merge/(1000*length_del_merge)),
             zwfl_internal_wk1 = (weight_wk1/(1000*length_wk1) - mean(weight_wk1/(1000*length_wk1)))/sd(weight_wk1/(1000*length_wk1)),
             zwfl_internal_wk3 = (weight_wk3/(1000*length_wk3) - mean(weight_wk3/(1000*length_wk3)))/sd(weight_wk3/(1000*length_wk3)),
             zwfl_internal_wk6 = (weight_wk6/(1000*length_wk6) - mean(weight_wk6/(1000*length_wk6)))/sd(weight_wk6/(1000*length_wk6)),
@@ -404,8 +375,8 @@ wfl_internal_zscores<- blood_child_complete %>%
             zwfl_internal_m18 = (weight_m18/(1000*length_m18) - mean(weight_m18/(1000*length_m18)))/sd(weight_m18/(1000*length_m18)))
 
 ## combine all datasets and add 
-blood_child_who_stan<- data.frame(blood_child_complete[,c("Subject_ID", weight_CN, length_CN, child_age_CN, "bw_sex", "bw_birth_GA")],
-                                  blood_child_who_stan_del,
+blood_child_who_stan_complete<- data.frame(blood_child_complete[,c("Subject_ID", weight, length, child_age, "bw_sex", "bw_birth_GA")],
+                                  blood_child_who_stan_del_merge,
                                   blood_child_who_stan_wk1,
                                   blood_child_who_stan_wk3,
                                   blood_child_who_stan_wk6,
@@ -417,53 +388,19 @@ blood_child_who_stan<- data.frame(blood_child_complete[,c("Subject_ID", weight_C
 
 
 
-# write_csv(blood_child_who_stan, "C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/S-PRESTO/input/anthropometry/blood_child_who_stan_complete_05162023.csv")
+# write_csv(blood_child_who_stan_complete, "C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/S-PRESTO/input/anthropometry/blood_child_who_stan_complete_05262023.csv")
 
 
 
-####################################################################
-####################################################################
-####################################################################
-
- blood_child_imputed<-read_csv("C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/S-PRESTO/input/anthropometry/blood_child_who_stan_04172023.csv")
- blood_child_complete<-read_csv("C:/Users/yaom03/OneDrive - The Mount Sinai Hospital/Documents/Projects/S-PRESTO/input/anthropometry/blood_child_who_stan_complete_05162023.csv")
-
-
-P<- blood_child_imputed %>% 
-    full_join(blood_child_complete, by="Subject_ID") %>% 
-    na.omit()
-
-cor(P$zwfl_internal_del_CN.x, P$zwfl_internal_del_CN.y)
-cor(P$zwfl_internal_wk1.x, P$zwfl_internal_wk1.y)
-cor(P$zwfl_internal_wk3.x, P$zwfl_internal_wk3.y)
-cor(P$zwfl_internal_wk6.x, P$zwfl_internal_wk6.y)
-cor(P$zwfl_internal_m3.x, P$zwfl_internal_m3.y)
-cor(P$zwfl_internal_m6.x, P$zwfl_internal_m6.y)
-cor(P$zwfl_internal_m12.x, P$zwfl_internal_m12.y)
-cor(P$zwfl_internal_m18.x, P$zwfl_internal_m18.y)
 
 
 
-data.frame(P$zwfl_internal_del_CN.x, P$zwfl_internal_del_CN.y)
 
-# sapply(blood_child_who_stan, function(x) sum(is.na(x)))
-# 
-# 
-# blood_child_check<- blood_child_who_stan %>%
-#                     inner_join((blood_child_imputed %>% 
-#                                 select(-starts_with("Days_since_birth"))), by="Subject_ID") %>%
-#                     filter(is.na(zwfl_del)==TRUE | is.na(zwfl_wk1)==TRUE) %>% 
-#                     select(Subject_ID, ends_with("_del"), ends_with("_wk1"))
-# 
-# # %>%
-# #                     select(Subject_ID,zwfl_del, zwfl_wk1, length_del, length_wk1)
-# 
-# anthro_zscores(
-#    sex = 1, age = 92,
-#    weight = 2.8, lenhei = 44.99999999999999, measure="L" , armc=10.85)
-# 
-# 
-# 
-# 
-# 
-# 
+
+
+
+
+
+
+
+
